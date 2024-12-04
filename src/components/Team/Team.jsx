0a7701b2b55ca2e4fb1eccd3d3e977dcbda5f737
danny8823 <dannyyoo714@gmail.com>
2024-12-04 11:56:42 -0800

    Added player per team data

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useParams,useLocation } from 'react-router-dom'
 import teams from '../../utls/team_data'
+
 export const Team = () => {
     const {id} = useParams()
+    const location = useLocation()
+    const {team} = location.state || {}
+    const data = teams.flat().find(team=>team.id === Number(id))
 
-    console.log(teams[0])
 
-    const find_team
+       
     return (
-        <div>Team</div>
+        <div>
+            <h1>{team?.name}</h1>
+            <p>Total games played:{data?.games}</p>
+            <p>Total assists: {data?.assists}</p>
+            <p>Total blocks: {data?.blocks}</p>
+        </div>
     )
 }
